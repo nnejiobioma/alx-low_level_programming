098ddbd8023e0cd3488a4c601cc3865a8dc67e44
nnejiobioma <nnejiobioma@gmail.com>
2022-12-13 08:04:20 -0800

    modified 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,33 @@
 #include "main.h"
+
 /**
-*main - prints _putchar, followed by a new line.
-*The program should return 0
-*Return: Always return 0 (success)
+*main - prints _putchar, fllowd by a new linees
+*Return: Alwys 0 (Success)
+*
 */
+
 int main(void)
+
 {
+
 	char abe[] = "_putchar";
+
+
 
 	int c;
 
+
+
 	for (c = 0; c < 8; c++)
+
 	{
-		_putchar(abe[c]);
+
+	_putchar(abe[c]);
+
 	}
+
 	_putchar('\n');
+
 	return (0);
+
 }
