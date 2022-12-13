f6e8273a9d92533739df3030c2566c88dc1bf7ef
nnejiobioma <nnejiobioma@gmail.com>
2022-12-13 18:22:59 +0000

    task two

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints the alphabet, in lowercase,
+*Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+	_putchar(d);
+	}
+	_putchar('\n');
+}
