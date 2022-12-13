8abf55d87b24ed9daf64f877a78636eb2da572bb
nnejiobioma <nnejiobioma@gmail.com>
2022-12-13 20:47:11 +0000

    1oth task complete

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*add - adds two integers
+*@x1: integer 1
+*@x2: integer 2
+*Return: sum
+*/
+
+int add(int x1, int x2)
+
+{
+	int sum;
+
+	sum = x1 + x2;
+
+	return (sum);
+}
