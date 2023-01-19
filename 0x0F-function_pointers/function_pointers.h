1a88c3e2116591edf92de9ac4f549e6fb95de6ce
nnejiobioma <nnejiobioma@gmail.com>
2023-01-19 19:18:04 +0000

    made changes to funcyion_ponters.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,11 @@
-#ifndef ASSIGN_0x0E
-#define ASSIGN_0x0E
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
 #include <stddef.h>
+
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int _putchar(char c);
 
-#endif
+#endif /* #ifndef FUNCTION_POINTERS_ */
