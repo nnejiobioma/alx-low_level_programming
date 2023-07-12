daee494a755aa952e7945614d338bd3c723827dd
obiomanneji <nneji.obioma@gmail.com>
2023-07-12 11:48:35 -0700

    made changes to task 103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,18 +1,18 @@
 #include "search_algos.h"
 
 /**
- * Description: Prints the [sub]array being searched after each change.
- * @value: The value to search for.
- * _binary_search - Searches for a value in a sorted array
- *                  of integers using binary search.
- * @array: A pointer to the first element of the array to search.
- * @right: The ending index of the [sub]array to search.
- *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the index where the value is located.
- * @left: The starting index of the [sub]array to search.
- *
- */
+  * _binary_search - Searches for a value in a sorted array
+  *                  of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int _binary_search(int *array, size_t left, size_t right, int value)
 {
 	size_t i;
@@ -40,17 +40,17 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 }
 
 /**
- * Description: Prints a value every time it is compared in the array.
- * exponential_search - Searches for a value in a sorted array
- *                      of integers using exponential search.
- * @value: The value to search for.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the index where the value is located.
- *
- */
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 0, right;
